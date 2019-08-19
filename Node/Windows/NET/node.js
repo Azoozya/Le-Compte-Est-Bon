f3b578a5839d5845b7ebe6f5973545ceff57eb9e
Azoozya <50519502+Azoozya@users.noreply.github.com>
2019-08-19 17:24:28 +0200

    add+

--- Node/Windows/NET/node.js
+++ Node/Windows/NET/node.js
@@ -3,6 +3,8 @@ var addon = require('Lceb');
   var express = require('express');
   var util = require('util');
   var handle = express();
+  handle.use(express.json()); // for parsing application/json
+  handle.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
@@ -12,7 +14,19 @@ var addon = require('Lceb');
   },function(req,res)
   {
     //On génère le nom de fichier
+    var number =  getRandomInt(10000);
+    var string = number.toString();
+    console.log('%d',number);
     //On crée le fichier avec le nom  qu'on vient de générer
+
+
+    //En cas d'erreur
+    fs.close(fd, (err) => {
+        if (err) throw err;
+                          });
+    console.log('Request Ended');
+    res.end('{"msg": "OK"}');
+    // res.end('<a href="http://www.google.com">Loutre</a>');
   });
   handle.listen(65535,function()
   {
@@ -20,3 +34,4 @@ var addon = require('Lceb');
   }                             );
 
 
+  });
